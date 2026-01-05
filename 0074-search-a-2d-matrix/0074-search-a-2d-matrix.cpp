afd076d78fb49a1b49cce57fc029fdcab930deb5
Shreyansh Raj <rajshreyansh546@gmail.com>
2026-01-05 23:38:22 +0530

    Time: 0 ms (100%), Space: 13.3 MB (75.27%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,6 +5,7 @@ public:
         int sti = 0, endi = matrix[0].size()-1;
         while(st<=end){
             int mid = end -(end-st)/2;
+            int sti = 0, endi = matrix[0].size()-1;
             if(matrix[mid][sti]<=target && matrix[mid][endi]>= target){
                 while(sti<=endi){
                     int midi = endi-(endi-sti)/2;
@@ -12,6 +13,7 @@ public:
                     else if(matrix[mid][midi]>=target) endi = midi-1;
                     else sti = midi+1;
                 }
+                return false;
             }
             else if(matrix[mid][sti]>=target) end = mid -1;
             else st = mid +1;
