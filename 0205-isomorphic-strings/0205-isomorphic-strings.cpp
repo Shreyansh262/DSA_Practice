b1138eab4fc4624a6c4b389a5c1ad454413254e1
Shreyansh Raj <rajshreyansh546@gmail.com>
2026-01-07 12:30:39 +0530

    Time: 2 ms (47.02%), Space: 9.3 MB (79.08%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_set <long long> a;
+        unordered_set <int> b,c;
+        if(s.size() != t.size()) return false;
+        for(int i = 0;i<s.size();i++){
+            long long key = ((long long)s[i] << 32) | t[i]; 
+            a.insert(key);
+            b.insert(s[i]);
+            c.insert(t[i]);
+        }
+        if(a.size()==b.size()&&a.size()==c.size()) return true;
+        else return false;
+
+    }
+};
