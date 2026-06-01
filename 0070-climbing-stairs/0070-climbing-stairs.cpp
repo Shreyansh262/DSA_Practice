32ab9c89d34ba6b57a1530df3912ad677d13099f
Shreyansh Raj <rajshreyansh546@gmail.com>
2026-06-02 01:26:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (33.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
+//Tabulization
     int climbStairs(int n) {
         vector<int> a(n+1,-1);
-        int ans = f(n,a);
-        return ans;
+        a[0]=1;
+        a[1]=1;
+        for(int i = 2;i<=n;i++){
+            a[i] = a[i-1]+a[i-2];
+        }
+        return a[n];
     }
-    int f(int n, vector<int>& x) {
-        if (n == 0) return 1;
-        if (n == 1) return 1;
-        if (x[n] != -1) return x[n];
-        int left = f(n - 1, x);
-        int right = f(n - 2, x);
-        x[n] = left + right;
-        return x[n];
-    }
+    //memoization
+    // int f(int n, vector<int>& x) {
+    //     if (n == 0) return 1;
+    //     if (n == 1) return 1;
+    //     if (x[n] != -1) return x[n];
+    //     int left = f(n - 1, x);
+    //     int right = f(n - 2, x);
+    //     x[n] = left + right;
+    //     return x[n];
+    // }
 };
