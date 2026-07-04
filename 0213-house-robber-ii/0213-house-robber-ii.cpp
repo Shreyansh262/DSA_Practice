1f7556420539dcc20264d0548e7f90a10f45854c
Shreyansh Raj <rajshreyansh546@gmail.com>
2026-07-05 01:42:34 +0530

    Time: 0 ms (100%), Space: 10.4 MB (62.48%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()==1){return nums[0];}
-        if(nums.size()==2){return max(nums[0],nums[1]);}
-        vector<int> dp(nums.size(), -1);
-        int left = cash(nums.size()-1,true,nums,dp);
-        vector<int> de(nums.size(), -1);
-        int right = cash(nums.size()-2,false,nums,de);
-        return max(left,right);
+        vector<int> temp;
+        vector<int> temp2;
+        int n = nums.size();
+        if(n==1) return nums[0];
+        for(int i = 0;i<n;i++){
+            if(i!=0) temp.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
+        }
+        return max(robber(temp),robber(temp2));
     }
-    int cash(int pos,bool cy ,vector<int>& nums, vector<int>& db){
-        if(pos <0) return 0;
-        if(pos == 0 && cy) return nums[0]-nums[nums.size()-1];
-        if(pos == 0 || pos == 1){return nums[pos];}
-        if(db[pos]!=-1) return db[pos];
-        int left = cash(pos - 2,cy,nums,db) + nums[pos];
-        int right = cash(pos - 3,cy,nums,db)+ nums[pos];
-        return db[pos] = max(left,right);
+    int robber(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+        int curr;
+        for(int i = 1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=prev2;
+            int notTake = 0+prev;
+            curr = max(take,notTake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
+
+
+    // if(nums.size()==1){return nums[0];}
+    //     if(nums.size()==2){return max(nums[0],nums[1]);}
+    //     vector<int> dp(nums.size(), -1);
+    //     int left = cash(nums.size()-1,nums,dp);
+    //     int right = cash(nums.size()-2,nums,dp);
+    //     return max(left,right);
+    // }
+    // int cash(int pos,vector<int>& nums, vector<int>& db){
+    //     if(pos <0) return 0;
+    //     if(pos == 0 || pos == 1){return nums[pos];}
+    //     if(db[pos]!=-1) return db[pos];
+    //     int left = cash(pos - 2,nums,db) + nums[pos];
+    //     int right = cash(pos - 3,nums,db)+ nums[pos];
+    //     return db[pos] = max(left,right);
 };        
