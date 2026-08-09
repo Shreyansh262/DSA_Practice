fafca4f18b6a012b2e8e9d0937d805e6bff58a19
Shreyansh Raj <rajshreyansh546@gmail.com>
2026-08-09 20:12:21 +0530

    Time: 7 ms (77.21%), Space: 13.6 MB (64.48%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    int solve(int ind, int M, vector<int>& piles,vector<vector<int>>&dp) {
-        if ((ind + 2 * M) >= piles.size()) {
-            int Z = 0;
-            for (int i = ind; i < piles.size(); i++) {
-                Z += piles[i];
-            }
-            return Z;
-        }
+    int solve(int ind, int M, vector<int>& piles,vector<vector<int>>&dp,vector<int>&suffix) {
+        int n = piles.size();
+        if(ind>=n) return 0;
+        if ((ind + 2 * M) >= n) return (suffix[ind]);
         if(dp[ind][M]!= INT_MIN) return dp[ind][M];
         int delta = 0;
         int maxProf = INT_MIN;
         for (int i = 1; i <= (2 * M); i++) {
             int temp_M = max(M, i);
-            int Z = 0;
-            for (int j = 0; j < i; j++) {
-                Z += piles[ind + j];
-            }
-            delta = Z - solve(ind + i, temp_M, piles,dp);
+            int Z = suffix[ind] - suffix[ind + i];
+            delta = Z - solve(ind + i, temp_M, piles,dp,suffix);
             maxProf = max(maxProf, delta);
         }
         return dp[ind][M] = maxProf;
     }
     int stoneGameII(vector<int>& piles) {
-        int sum = accumulate(piles.begin(), piles.end(), 0);
-        vector<vector<int>>dp(piles.size(),vector<int>(piles.size()+1,INT_MIN));
-        int AliceGain = solve(0, 1, piles,dp);
-        return (sum + AliceGain) / 2;
+        int n = piles.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,INT_MIN));
+        vector<int>suffix(n+1,0);
+        for (int i = n - 1; i >= 0; i--)
+            suffix[i] = piles[i] + suffix[i + 1];
+        int AliceGain = solve(0, 1, piles,dp,suffix);
+        return (suffix[0] + AliceGain) / 2;
     }
 };
